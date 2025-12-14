ad22575720a21950595ca0c992d1e3b6582e1d59
clouddecode <52980013+clouddecode@users.noreply.github.com>
2025-12-13 22:30:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,10 @@ time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
-
+  
+st.success(f'You {name_on_order} Your Smoothie is ordered!', icon="✅")
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    st.success(f'You {name_on_order} Your Smoothie is ordered!', icon="✅")
+    
